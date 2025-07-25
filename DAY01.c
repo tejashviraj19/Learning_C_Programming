16ecd4be63bc80794c3af003a0d80d09f7b7273b
Tejashvi Raj <tejashvirajyadav192028@gmail.com>
2025-07-25 17:49:58 +0530

    Create DAY01.c

---
+++ DAY01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int Tejash;
+    Tejash=19;
+    printf("Jersey number of Tejashvi is %d." ,Tejash);
+    return 0;
+}
+
+//Both code works same!!
+
+#include<stdio.h>
+
+int main(){
+    int Tejash=19;
+    printf("Jersey number of Tejashvi is %d.",Tejash);
+    return 0;
+}
